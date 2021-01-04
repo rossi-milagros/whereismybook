ccd735dfc00ea122bed7d4990d580d5589447a0d
Ignacio de Villafañe <idevillafane@gmail.com>
2021-01-04 03:11:49 -0300

    Update persona.js

--- persona.js
+++ persona.js
@@ -1,24 +1,10 @@
-var app = require('express').Router();
-var persona = require('../persona');
-
-
-/**
- API de Persona
- *  POST '/persona'
- *  GET '/persona'
- *  GET '/persona/:id'
- *  PUT '/persona/:id'
- *  DELETE '/persona/:id'
- */
-
-
-
+//API de Personas
 
 /* Agregar una persona:
 POST '/persona' recibe: {nombre: string, apellido: string, alias: string, email: string} retorna: status: 200, {id: numerico, nombre: string, apellido: string, alias: string, email: string} - status: 413, {mensaje: <descripcion del error>} que puede ser: "faltan datos", "el email ya se encuentra registrado", "error inesperado"
 */
 
-app.post('/persona', async (req, res)) => {
+app.post('/persona', async (req, res) =>{
     try {
         if (!req.body.nombre || !req.body.apellido || !req.body.email || !req.body.alias) {
             throw new Error("Faltan datos");
@@ -26,24 +12,26 @@ app.post('/persona', async (req, res)) => {
 
         // Con estas lineas se deberia poder chequear si ya esta registrado el email:
 
-        var chequearEmail = 'SELECT * FROM persona WHERE email = ?';
-        let respuesta = qy(query, [req.body.email]);
+        query = 'INSERT INTO persona (nombre, apellido, email, alias) VALUES (?, ?, ?, ?)';
+
+        let chequearEmail = 'SELECT * FROM persona WHERE email = ?';
+        let respuesta = await qy(chequearEmail, [req.body.email.toUpperCase()]);
 
         if (respuesta.length > 0) {
             throw new Error("El email ya se encuentra registrado");
-        };
+        }
     
-        query = 'INSERT INTO producto (nombre, apellido, email, alias) VALUES (?, ?, ?, ?)';
+        console.log({"respuesta" : respuesta});
 
-        respuesta = await qy(query, [req.body.nombre.toUpperCase(), req.body.apellido.toUpperCase(), req.body.email.toUpperCase(), req.body.alias.toUpperCase());
+        respuesta = await qy(query, [req.body.nombre.toUpperCase(), req.body.apellido.toUpperCase(), req.body.email.toUpperCase(), req.body.alias.toUpperCase()]);
 
-        res.send({'respuesta': respuesta.insertId}); //No se si esta línea se escribe así. Lo copie del TP. No sé tampoco si hace falta poner un identificador, creo que no.
+        res.send({'respuesta': "Hay "+respuesta.insertId+" persona(s) registrada(s)"}); //No se si esta línea se escribe así. Lo copie del TP. No sé tampoco si hace falta poner un identificador, creo que no.
         
     }
 
     catch(e) {
         console.error(e.message);
-        res.status(413).send({"Error inesperado": e.message}); //Revisar dónde se coloca el mensaje "personalizado"
+        res.status(413).send({"Error": e.message}); //Revisar dónde se coloca el mensaje "personalizado"
     }
 });
 
@@ -52,25 +40,25 @@ Pedir lista de personas:
 GET '/persona' retorna status 200 y [{id: numerico, nombre: string, apellido: string, alias: string, email; string}] o bien status 413 y []
 */
 
-app.get ('/persona', async (req, res)) => {
+app.get ('/persona', async (req, res) => {
     try {
-        var query = 'SELECT * FROM producto';
+        var query = 'SELECT * FROM persona';
 
-        var respuesta = query(query);
+        var respuesta = await qy(query);
         res.send({'respuesta': respuesta});
     }
     catch(e) {
         console.error(e.message);
-        res.status(413).send({"Error inesperado": e.message};)
+        res.status(413).send({"Error": e.message});
     }
+});
 
 /* GET '/persona/:id' retorna status 200 y {id: numerico, nombre: string, apellido: string, alias: string, email; string} - status 413 , {mensaje: <descripcion del error>} "error inesperado", "no se encuentra esa persona" */
 
-app.get ('/persona/:id', async (req, res)) => {
+app.get ('/persona/:id', async (req, res) => {
     try {
         const query = 'SELECT * FROM persona WHERE id = ?';
 
-        // const respuesta = await qy()...
         const respuesta = await qy(query, [req.params.id]);
 
         console.log(respuesta);
@@ -82,29 +70,44 @@ app.get ('/persona/:id', async (req, res)) => {
         res.status(413).send({"Error": e.message});
     }
 
-};
+});
 
 /*PUT '/persona/:id' recibe: {nombre: string, apellido: string, alias: string, email: string} el email no se puede modificar. retorna status 200 y el objeto modificado o bien status 413, {mensaje: <descripcion del error>} "error inesperado", "no se encuentra esa persona"*/ 
 
-app.put('/persona/:id', async (req, res) {
-    try{
+app.put('/persona/:id', async (req, res) => {
+    try {
         
-    if (!req.body.nombre & !req.body.apellido & !req.body.email & !req.body.alias) {
-        throw new Error("No hay datos que actualizar");
-    }
-    let existe = qy(query, [req.params.id])
+        if (!req.body.nombre & !req.body.apellido & !req.body.email & !req.body.alias) {
+            throw new Error("No hay datos que actualizar");
+        }
 
-    if (existe.id = null){
-        throw new Error("Esa Persona no existe");
-    }
+        let query = 'SELECT * FROM persona WHERE id = ?';
 
-    let respuesta = qy(query, [req.body]);
+        let existe = await qy(query, [req.params.id]);
 
-    var query = 'UPDATE persona SET ( nombre = ?, apellido = ?, email = ?, alias = ?) WHERE id = ?'
+        if (existe.lenght = 0) {
+            throw new Error("Esa Persona no existe");
+        }
 
-    respuesta = await qy((query, [req.body.nombre.toUpperCase(), req.body.apellido.toUpperCase(), req.body.email.toUpperCase(), req.body.alias.toUpperCase(),req.params.id]));
+        // Comprobamos que el email esté registrado y que corresponda al id ingresado. De no cumplirse ambas condiciones, arrojamos error con msj: 'El email no se puede modificar'.
 
-    } catch (e) {
+        let chequearEmail = 'SELECT * FROM persona WHERE email = ? && id = ?';
+        let respuestaEmail = await qy(chequearEmail, [req.body.email.toUpperCase(), req.params.id]);
+
+        if (respuestaEmail.length == 0) {
+            throw new Error("El email no se puede modificar");
+        }
+
+        // Cambiamos los datos
+
+        query = 'UPDATE persona SET nombre = ?, apellido = ?, email = ?, alias = ? WHERE id = ?';
+
+        respuesta = await qy(query, [req.body.nombre.toUpperCase(), req.body.apellido.toUpperCase(), req.body.email.toUpperCase(), req.body.alias.toUpperCase(), req.params.id]);
+
+        res.send({"respuesta": respuesta.affectedRows});
+
+    } 
+    catch (e) {
         console.error(e.message);
         res.status(413).send({"Error": e.message});  
     }
@@ -112,83 +115,34 @@ app.put('/persona/:id', async (req, res) {
 });  
 
 /*DELETE '/persona/:id' retorna: 200 y {mensaje: "se borro correctamente"} o bien 413, {mensaje: <descripcion del error>} "error inesperado", "no existe esa persona", "esa persona tiene libros asociados, no se puede eliminar" */
-app.delete('/persona/:id'), async (req, res) {
+
+app.delete('/persona/:id', async (req, res) => {
     try {
-        let existe = qy(query, [req.params.id])
-        if (existe.id = null){
-            throw new Error("Esa Persona no existe");
-        }
+        
+        let query = 'SELECT * FROM persona WHERE id = ?';
 
-        let respuesta = qy(query, [req.params.id]);
-        var query = 'DELETE FROM persona WHERE id = ?'
-        respuesta = qy((query, [req.params.id]));
+        let existe = await qy(query, [req.params.id]);
 
-        res.send({"respuesta": respuesta});
-    } catch (e) {
+        if (!([existe.length] > 0)) {
+            throw new Error("no existe esa persona");
+        };
+
+        query = 'SELECT * FROM libro WHERE persona_id = ?';
+
+        let respuestaLibro = await query(query, [req.params.id]);
+
+        if ([respuestaLibro.length > 0]) {
+            throw new Error("esa persona tiene libros asociados, no se puede eliminar");
+        };
+        
+        query = 'DELETE FROM persona WHERE id = ?';
+
+        let respuesta = await qy(query, [req.params.id]);
+
+        res.status(200).send({"mensaje" : "se borro correctamente"});
+    }
+    catch (e) {
         console.error(e.message);
         res.status(413).send({"Error": e.message}); 
     }
-}
-
-
-/* Lo que sigue es lo que hizo la profesora */
-
-
-var nombre = req.body.nombre;
-var instancia = new ArtistaModel({ nombre: nombre });
-instancia.save((err, artista) => {
-    if (err) {
-        next(new Error('No se pudo guardar el artista'));
-    }
-    res.status(201).send(artista); // Codigo HTTP 201 es creado
 });
-});
-
-
-// Listado de personas
-app.get('/', function (req, res, next) {
-    // Es el mismo codigo del listado de artistas normal (pero sin interfaz)
-    ArtistaModel.find({}, (err, listado) => {
-        if (err) {
-            next(new Error('No se pudieron obtener los artistas'));
-            return;
-        }
-        res.send(listado);
-    });
-});
-
-// Obtener un artista
-app.get('/:id', function (req, res, next) {
-    var idArtista = req.params.id;
-    ArtistaModel.findById(idArtista, (err, artista) => {
-        if (err) {
-            next(new Error('No se encontro el artista'));
-        }
-        res.send(artista);
-    });
-});
-
-// Actualizar un artista
-app.put('/:id', function (req, res, next) {
-    var idArtista = req.params.id;
-    var nombreNuevo = req.body.nombre;
-    ArtistaModel.findByIdAndUpdate(idArtista, { nombre: nombreNuevo }, (err, artista) => {
-        if (err) {
-            next(new Error('No se pudo actualizar el artista'));
-        }
-        res.send(artista);
-    })
-});
-
-// Borrar un artista
-app.delete('/:id', function (req, res, next) {
-    var idArtistaABorrar = req.params.id;
-    ArtistaModel.findByIdAndRemove(idArtistaABorrar, (err, artista) => {
-        if (err) {
-            next(new Error('No se pudo borrar el artista'));
-        }
-        res.status(204).send(); // Codigo HTTP 204 Sin contenido (no hay nada que responder, pero la operacion fue exitosa)
-    });
-});
-
-module.exports = app;
